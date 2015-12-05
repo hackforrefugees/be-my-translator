14ed20e3ca17770c38466d44710c2a048daffc30
William Johansson <radar@radhuset.org>
2015-12-05 19:31:33 +0100

    Question view with database fetching
    Fetches questions from the MongoDB databases, and for each question prints the
    question type, text and the answers.
    
    Also makes it possible to mark an answer as 'approved', by storing the ObjectID
    of the answer in an 'approved' field in the question.

--- client/question_details.js
+++ client/question_details.js
@@ -35,4 +35,26 @@ if (Meteor.isClient) {
         ]
         */
     });
+
+    Template.question_answer.events({
+        "click .answered": function (event, template) {
+            var parent = Template.parentData();
+            Questions.update(
+                { _id: parent._id },
+                { $set: { approved: template.data._id } }
+            );
+        },
+    });
+
+    Template.question_approved.helpers({
+        answer: function () {
+            // Get just that single matching item
+            var item = Questions.findOne(
+                {"answers._id": this.item},
+                {"_id": 0, "answers.$": 1}).answers[0];
+            // Make sure question_answer now this is an approved answer
+            item.approved = true;
+            return item;
+        }
+    });
 }
