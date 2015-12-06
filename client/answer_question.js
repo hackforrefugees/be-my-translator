e4b142e0b641e1ea9d223dc605f1d5bc1714bb9a
Sara <sayo.panda.sn@gmail.com>
2015-12-06 11:37:57 +0100

    Some UI

--- client/answer_question.js
+++ client/answer_question.js
@@ -1,5 +1,23 @@
 if (Meteor.isClient) {
     // code to run in client
+
+   /* Template.questionForm.events({
+        'submit .answer_question_text': function(event){
+                event.preventDefault();
+
+                Questions.insert({
+                    user: "Dummy User 2",
+                    question: questionId, //Should be getted from the question
+                    timestamp: new Date(),
+                    type: "text",
+                    data: event.target.text.value,
+                });
+            },
+            'click .go-to-record': function(event) {
+                event.preventDefault();
+                Router.go('/record');
+            },
+    });*/
 }
 if (Meteor.isServer) {
   Meteor.startup(function () {
