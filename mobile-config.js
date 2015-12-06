a932a6814f2b89467a78984c7ad13b6879bf0e92
Sara <sayo.panda.sn@gmail.com>
2015-12-06 16:04:14 +0100

    Splash and no portrait fixed!

--- mobile-config.js
+++ mobile-config.js
@@ -17,14 +17,25 @@ App.icons({
   'android_hdpi': 'public/images/bmt-logo_72x72.png',
   'android_xhdpi': 'public/images/bmt-logo_96x96.png'
 });
-/*
+
 App.launchScreens({
   // iOS
   //'iphone': 'resources/splash/splash-320x480.png',
 
   // Android
- // 'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-});*/
+ 'android_ldpi_portrait': 'public/images/bmt-splash_200x320.png',
+ //'android_ldpi_landscape': 'public/images/bmt-splash_200x320.png',
+ 'android_mdpi_portrait': 'public/images/bmt-splash_320x480.png',
+// 'android_mdpi_landscape': 'public/images/bmt-splash_320x480.png',
+ 'android_hdpi_portrait': 'public/images/bmt-splash_480x800.png',
+ //'android_hdpi_landscape': 'public/images/bmt-splash_480x800.png',
+ 'android_xhdpi_portrait': 'public/images/bmt-splash_720x1280.png',
+ //'android_xhdpi_landscape': 'public/images/bmt-splash_720x1280.png',
+});
 
+// Set PhoneGap/Cordova preferences
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('Orientation', 'portrait');
+
